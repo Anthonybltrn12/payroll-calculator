45157c4bcdddb4b180c4b74ea7b66329ceadcb3c
antbeltran <anthonybltrn@gmail.com>
2026-04-09 15:41:30 -0500

    finished without bonus

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,28 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String userName = myScanner.nextLine();
+
+
+
+        System.out.print("How many hours did you work this pay-period?");
+        int hoursWorked = myScanner.nextInt();
+
+        System.out.print("What is your hourly rate?");
+        double hourlyRate = myScanner.nextDouble();
+
+        myScanner.nextLine();
+
+        System.out.println(userName + " makes $" + (hoursWorked * hourlyRate) + " per check");
+
+
+    }
 }
